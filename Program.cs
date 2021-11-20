bcaa7e9c27a25a3101bfbae8439977ab4ea7ef35
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2021-11-19 22:41:53 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Azure.Functions.Worker.Configuration;
+
+namespace azure_functions_on_digitalocean_app_platform
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .RunConsoleAsync();
+
+        }
+    }
+}
